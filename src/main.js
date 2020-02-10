d3e0e4bc1b52d39494b2b79dac728370296efd65
tyler <drelyt@gmail.com>
2020-02-10 16:03:45 -0500

    removed iife

--- src/main.js
+++ src/main.js
@@ -2,21 +2,19 @@ import { init } from "./ui";
 import { calculateResistance } from "./resistor";
 import ResistorDisplay from "./resistor_display";
 
-(function main() {
-  // create the resistor display and attach it to
-  // the container.
-  const resistorDisplay = new ResistorDisplay(
-    document.querySelector("#resistor-container")
-  );
+// create the resistor display and attach it to
+// the container.
+const resistorDisplay = new ResistorDisplay(
+  document.querySelector("#resistor-container")
+);
 
-  // init requires a function that will be called whenever the
-  // resistor is changed.
-  // resistor will be an instance of the Resistor class.
-  init(resistor => {
-    setResistorValue(resistor);
-    resistorDisplay.display(resistor);
-  });
-})();
+// init requires a function that will be called whenever the
+// resistor is changed.
+// resistor will be an instance of the Resistor class.
+init(resistor => {
+  setResistorValue(resistor);
+  resistorDisplay.display(resistor);
+});
 
 function setResistorValue(resistor) {
   const val = calculateResistance(resistor);
